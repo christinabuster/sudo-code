d42ea8f3408df8e41fc4c1c27930836b9e71b2c5
christinabuster <christina.buster13@gmail.com>
2018-10-19 16:56:10 -0700

    This is a sudo application for a code challenge

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,34 @@
+# Controller
+
+class People < ActionController::Base
+
+	# ... Other REST actions
+
+	def create
+		@person = Person.new(params[:person])
+
+		slug = "ABC123#{Time.now.to_i.to_s}1239827#{rand(10000)}"
+		@person.slug = slug
+		@person.admin = false
+
+		if (Person.count + 1).odd?
+			team = "UnicornRainbows"
+			handle = "UnicornRainbows" + (Person.count + 1).to_s
+			@person.handle = handle
+			@person.team = team
+		else
+			team = "LaserScorpions"
+			handle = "LaserScorpions" + (Person.count + 1).to_s
+			@person.handle = handle
+			@person.team = team
+		end
+
+		if @person.save
+			Emails.validate_email(@person).deliver
+			@admins = Person.where(:admin => true)
+			Emails.admin_new_user(@admins, @person).deliver
+			redirect_to @person, :notice => "Account added!"
+		else
+			render :new
+		end
+	end
---
+++ app/models/application_record.rb
@@ -0,0 +1,5 @@
+# Model
+
+class Person < ActiveRecord::Base
+	attr_accessible :first_name, :last_name, :email, :admin, :slug, :validated, :handle, :team
+end
